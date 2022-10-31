ec6eb7be4d036947eee09c424c6b32d104e622a9
paulina <labbepaulina@gmail.com>
2022-10-30 22:00:07 -0300

    promedio

---
+++ script.js
@@ -0,0 +1,11 @@
+let nota1 = 6;
+let nota2 = 8;
+let nota3 = 2;
+let nota4= 5;
+let nota5= 10;
+let nota6= 9;
+let total_suma = nota1+nota2+nota3+nota4+nota5+nota6;
+console.log(total_suma)
+
+let promedio = total_suma/6
+console.log(promedio)
